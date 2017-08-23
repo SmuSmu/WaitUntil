16254532c060fcaf1fa548c31d9080262facf235
Jean Fiedler <JFI@kd.local>
2017-08-23 13:02:25 +0200

    Timout function & loop

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 // Extern Libary for command line arguments
 // Docs https://docs.rs/clap/2.26.0/clap/
 extern crate clap;
+extern crate time;
 use clap::{Arg, App, SubCommand};
  
 fn main() {
@@ -29,20 +30,34 @@ fn main() {
                           .get_matches();
  
     // Start
+    //let ten_millis = time::Duration::from_millis(10);
+
+    println!("Current Time : {} seconds", time::precise_time_s());
+    println!("Current Time : {} seconds", time::precise_time_s() + 30.0 as f64);
+
+    // default timeout is 30 sec
+    // Using f64 because precise_time_s() returns f64. So better for calculation later.
+    let mut varTimeOut = 30.0 as f64 ;
+
     if matches.is_present("timeout") {
-        if let Some(varTimeOut) = matches.value_of("timeout") {
-            println!("TimeOut setted to : {} seconds", varTimeOut);
+        // get timeout value. First as String(?)
+        if let Some(varArgTimeOut) = matches.value_of("timeout") {
+            // convert TimeOut to f64
+            varTimeOut = varArgTimeOut.parse::<f64>().unwrap();
             }
         }
-    else {
-        let varTimeOut = "30"; 
-        println!("Using Default Timeout of : {} seconds", varTimeOut);
-        }
+    
+    let varTimeOutTraget = time::precise_time_s() + varTimeOut;
 
+    println!("Timeout is setted to : {} seconds", varTimeOut);
 
     if matches.is_present("fileexists") {
         if let Some(varFileName) = matches.value_of("fileexists") {
             println!("Now Waiting for file : {}", varFileName);
+            while time::precise_time_s() < varTimeOutTraget {
+                println!("Current Time : {} seconds", time::precise_time_s());
+                //thread::sleep(ten_millis);
+                }
             }
         }
     else {
